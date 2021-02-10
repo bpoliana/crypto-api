c7de60b9cdcdd0be133c4b71b26c3f35bacc9309
Barbara Jaber <barbara.jaber@thoughtworks.com>
2021-02-10 00:25:28 -0300

    test: add test to LoginController when an invalid email is provided

--- src/controllers/login.spec.ts
+++ src/controllers/login.spec.ts
@@ -1,10 +1,32 @@
 import { LoginController } from './login'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
+import { EmailValidator } from '../protocols/email-validator'
+import { InvalidParamError } from '../errors/invalid-param-error'
+
+interface factoryTypes {
+  login: LoginController
+  emailValidatorStub: EmailValidator
+}
+
+const makeLogin = (): factoryTypes => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+
+  const emailValidatorStub = new EmailValidatorStub()
+  const login = new LoginController(emailValidatorStub)
+  return {
+    login,
+    emailValidatorStub
+  }
+}
 
 describe('Login Controller', () => {
   test('Should return 400 if no email is provided', () => {
-    const login = new LoginController()
+    const { login } = makeLogin()
     const request = {
       body: {
         password: 'password'
@@ -15,7 +37,7 @@ describe('Login Controller', () => {
   })
 
   test('Should return 400 if no password is provided', () => {
-    const login = new LoginController()
+    const { login } = makeLogin()
     const request = {
       body: {
         email: 'somebody@email.com'
@@ -23,5 +45,18 @@ describe('Login Controller', () => {
     }
     const response = login.handle(request)
     expect(response).toEqual(badRequest(new MissingParamError('senha')))
+  })
+
+  test('Should return 400 if an invalid email is provided', () => {
+    const { login, emailValidatorStub } = makeLogin()
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const request = {
+      body: {
+        email: 'invalid@email.com',
+        password: 'password'
+      }
+    }
+    const response = login.handle(request)
+    expect(response).toEqual(badRequest(new InvalidParamError('email')))
   })
 })
