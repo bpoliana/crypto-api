613a8ed7b89ecec0d49d626ba9a9933a4bfe1f39
Barbara Jaber <bpolianajaber@gmail.com>
2021-02-10 02:27:43 -0300

    refactor: move EmailValidator mock to a factory

--- src/services/email-validator-service.spec.ts
+++ src/services/email-validator-service.spec.ts
@@ -6,17 +6,28 @@ jest.mock('validator', () => ({
     return true
   }
 }))
+
+const makeEmailValidator = (): EmailValidatorService => {
+  return new EmailValidatorService()
+}
 describe('EmailValidator Service', () => {
   test('Should return false if validator returns false', () => {
-    const emailValidatorAdapter = new EmailValidatorService()
+    const emailValidatorAdapter = makeEmailValidator()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = emailValidatorAdapter.isValid('invalid_mail@mail.com')
     expect(isValid).toBe(false)
   })
 
   test('Should return true if validator returns true', () => {
-    const emailValidatorAdapter = new EmailValidatorService()
+    const emailValidatorAdapter = makeEmailValidator()
     const isValid = emailValidatorAdapter.isValid('valid_mail@mail.com')
     expect(isValid).toBe(true)
+  })
+
+  test('Should call validator with the same email', () => {
+    const emailValidatorAdapter = makeEmailValidator()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    emailValidatorAdapter.isValid('any@mail.com')
+    expect(isEmailSpy).toBeCalledWith('any@mail.com')
   })
 })
