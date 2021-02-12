2f9165499a35faed2bef88f40a328837393a7dd3
Barbara Jaber <bpolianajaber@gmail.com>
2021-02-12 00:14:24 -0300

    refactor: create helper for internal server error message

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -1,6 +1,6 @@
 import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, internalServerError } from '../helpers/http-helper'
 import { EmailValidator } from '../protocols/email-validator'
 import { InvalidParamError } from '../errors/invalid-param-error'
 import { ServerError } from '../errors/server-error'
@@ -25,10 +25,7 @@ export class LoginController {
         return badRequest(new InvalidParamError('email'))
       }
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: new ServerError()
-      }
+      return internalServerError(new ServerError())
     }
   }
 }
