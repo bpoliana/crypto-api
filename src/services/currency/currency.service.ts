a654edff9e69dca06f66f047f9c2718a28d42deb
Barbara Jaber <bpolianajaber@gmail.com>
2021-02-20 16:27:13 -0300

    feat: ensures GET route returns currencies based on most recent USD rate

--- src/services/currency/currency.service.ts
+++ src/services/currency/currency.service.ts
@@ -5,7 +5,42 @@ export class CurrencyService {
   async getCurrencies () {
     const coinDeskService = new CoinDeskClient()
     const btcResponse = await coinDeskService.getBtc()
-    await fs.writeFile('src/db/coin-desk-response.json', JSON.stringify(btcResponse, null, 2), 'utf8', () => {})
-    return btcResponse
+
+    fs.writeFileSync('src/db/coin-desk-response.json', JSON.stringify(btcResponse, null, 2))
+
+    const data = fs.readFileSync('src/db/currencies.json')
+    const currencies = JSON.parse(data.toString())
+
+    const { BRL, EUR, CAD } = currencies
+    const { USD } = btcResponse.bpi
+    const rateBTC = {
+      BRL: BRL * USD.rate_float,
+      EUR: EUR * USD.rate_float,
+      CAD: CAD * USD.rate_float
+    }
+    const currenciesBTCBased = {
+      BRL: {
+        code: 'BRL',
+        rate: rateBTC.BRL.toLocaleString(),
+        description: 'Brazilian Real',
+        rate_float: rateBTC.BRL
+      },
+      EUR: {
+        code: 'EUR',
+        rate: rateBTC.EUR.toLocaleString(),
+        description: 'Euro',
+        rate_float: rateBTC.EUR
+      },
+      CAD: {
+        code: 'CAD',
+        rate: rateBTC.CAD.toLocaleString(),
+        description: 'Canadian Dolar',
+        rate_float: rateBTC.CAD
+      }
+    }
+    return ({
+      ...btcResponse,
+      bpi: { ...btcResponse.bpi, ...currenciesBTCBased }
+    })
   }
 }
