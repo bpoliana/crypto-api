25c34c002c9a9e61c011132a19abc5e25a660e8c
Barbara Jaber <bpolianajaber@gmail.com>
2021-02-20 20:44:37 -0300

    refactor: ensure CurrencyController catch any error in getCurrencies method

--- src/controllers/currency/currency.controller.ts
+++ src/controllers/currency/currency.controller.ts
@@ -3,13 +3,13 @@ import { internalServerError, ok } from '../../helpers/http-helper'
 import { CurrencyService } from '../../services/currency/currency.service'
 
 class CurrencyController {
-  async handle (req: Request, res: Response) {
+  async getCurrencies (req: Request, res: Response) {
     try {
       const currencyService = new CurrencyService()
       const serviceResponse = await currencyService.getCurrencies()
       return ok(res, serviceResponse)
-    } catch (ServerError) {
-      return internalServerError(res, ServerError)
+    } catch (err) {
+      return internalServerError(res, err)
     }
   }
 }
