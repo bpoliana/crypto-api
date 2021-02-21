9d4bdd98c6ae54c226b370d1a8171f21dbe5d9bc
Barbara Jaber <bpolianajaber@gmail.com>
2021-02-21 01:05:07 -0300

    fix: ensure currency value is integer and greater than zero

--- src/middlewares/fields-validators.ts
+++ src/middlewares/fields-validators.ts
@@ -1,6 +1,6 @@
 
 import { check } from 'express-validator'
 
-export const currencyValidations = [check('currency').isIn(['BRL', 'EUR', 'CAD']), check('value').isNumeric()]
+export const currencyValidations = [check('currency').isIn(['BRL', 'EUR', 'CAD']), check('value').isInt({ gt: 0 })]
 
 export const loginValidations = [check('email').isEmail(), check('password').isLength({ min: 6, max: 6 }).isNumeric()]
