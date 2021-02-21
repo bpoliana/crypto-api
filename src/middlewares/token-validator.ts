749e237b022f64ed9745460d6a695a1cc2d9023a
Barbara Jaber <bpolianajaber@gmail.com>
2021-02-20 23:33:11 -0300

    feat: ensure routes /crypto/btc can only be used when token is valid

--- src/middlewares/token-validator.ts
+++ src/middlewares/token-validator.ts
@@ -4,18 +4,19 @@ import * as jwt from 'jsonwebtoken'
 import { jwtSecret } from '../config'
 
 export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers.auth
+  const token = req.headers.authorization
   let jwtPayload
   try {
     jwtPayload = jwt.verify(token.toString(), jwtSecret)
     res.locals.jwtPayload = jwtPayload
   } catch (error) {
-    res.status(401).send()
+    res.status(401).send({ message: 'Token inválido' })
     return
   }
 
-  const { email } = jwtPayload
-  const newToken = jwt.sign({ email }, jwtSecret, {
+  const { id } = jwtPayload
+  console.log(jwtPayload)
+  const newToken = jwt.sign({ id }, jwtSecret, {
     expiresIn: '1h'
   })
   res.setHeader('accessToken', newToken)
--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,14 @@ import { Router } from 'express'
 import AuthController from './controllers/auth/auth.controller'
 import CurrencyController from './controllers/currency/currency.controller'
 import { currencyValidations } from './middlewares/fields-validators'
+import { checkJwt } from './middlewares/token-validator'
 
 const routes = Router()
 
 routes.post('/login', AuthController.login)
 
-routes.get('/crypto/btc', CurrencyController.getCurrencies)
+routes.get('/crypto/btc', checkJwt, CurrencyController.getCurrencies)
 
-routes.post('/crypto/btc', currencyValidations, CurrencyController.updateCurrencies)
+routes.post('/crypto/btc', checkJwt, currencyValidations, CurrencyController.updateCurrencies)
 
 export { routes }
