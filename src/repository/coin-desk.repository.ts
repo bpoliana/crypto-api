b1f9604a0ab613ddb7ed51c565bec254abb0bdc4
Barbara Jaber <bpolianajaber@gmail.com>
2021-02-21 23:31:24 -0300

    fix: remove unnecessary repository

--- src/repository/coin-desk.repository.ts
+++
@@ -1,7 +0,0 @@
-import fs from 'fs'
-
-export class CoinDeskRepository {
-  save = (btcResponse: object) => {
-    fs.writeFileSync('src/db/coin-desk-response.json', JSON.stringify(btcResponse, null, 2))
-  }
-}
