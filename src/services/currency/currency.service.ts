70ede482e7f3971b5b97c7c3bcdba2cc1958fdb8
Barbara Jaber <bpolianajaber@gmail.com>
2021-02-21 23:29:04 -0300

    fix: remove unnecessary files

--- src/services/currency/currency.service.ts
+++ src/services/currency/currency.service.ts
@@ -1,5 +1,4 @@
 import { UpdateCurrencyDTO } from '../../dto/UpdateCurrencyDTO'
-import { CoinDeskRepository } from '../../repository/coin-desk.repository'
 import { CurrencyRepository } from '../../repository/currency.repository'
 import { buildBtcResponseDTO } from '../builders/btcResponseDTO.builder'
 import { CoinDeskClient } from '../client/coin-desk/coin-desk.client'
@@ -8,9 +7,6 @@ export class CurrencyService {
   async getCurrencies () {
     const coinDeskService = new CoinDeskClient()
     const btcResponse = await coinDeskService.getBtc()
-
-    const coinDeskRepository = new CoinDeskRepository()
-    coinDeskRepository.save(btcResponse)
 
     const currencyRepository = new CurrencyRepository()
     const currencies = currencyRepository.getCurrencies()
